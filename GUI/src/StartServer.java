910c89838f9cc816fa474ed3883375509f8ae623
Jeremy Borys <jnickborys@gmail.com>
2014-11-17 17:37:03 -0800

    Updated for thread

--- GUI/src/StartServer.java
+++ GUI/src/StartServer.java
@@ -1,3 +1,4 @@
+import java.awt.PageAttributes.OrientationRequestedType;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,71 +6,133 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import javax.swing.*;
 
 
 
 public class StartServer implements Runnable {
-
+	
 	public String name;
 	public ServerSocket server;
 	public int port, rem_port;
 	public String ip;
-
+	
+	public String rem_server = "127.0.0.1";
+	public static int thread_cnt = 1;
+	
 	public Socket producer;
 	
 	public StartServer(int local_port, int remote_port) {
 		
+		thread_cnt++;
+		name = String.format("THREAD-%d", thread_cnt);
 		port = local_port;
 		rem_port = remote_port;
 		System.out.println("Set port to: " + Integer.toString(port));
 	}
 	
+	private void updateGUI(final String t_update) {
+		// This adds a function to execute in the Event-Dispatch-Thread
+		// Avoiding any race conditions that could occur!
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				System.out.println(t_update);
+//				getContentPane().get
+//				orientation_Data.append(t_update);
+			}
+		});
+	}
+	
 	public void run() {
 
-		System.out.println("Starting server on port: " + Integer.toString(port));
+		updateGUI("test");
+		
+		System.out.printf("Starting server on port: %d\n", port);
 		String data = null;
 		Socket client = null;
 		PrintWriter printwriter = null;
+		BufferedReader in = null;
+		producer = null;
 		
 		try {
-			producer = new Socket("127.0.0.1", rem_port);
+			producer = new Socket(rem_server, rem_port);
 			printwriter = new PrintWriter(producer.getOutputStream(), true);
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+//			e.printStackTrace();
+			System.out.printf(
+				"%s:ERROR: Failed to connecto to server: %s:%d\n", name, rem_server, rem_port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+//			e.printStackTrace();
+			System.out.printf(
+				"%s:ERROR: Failed to connecto to server: %s:%d\n", name, rem_server, rem_port);
+		}
+
+		if (producer == null)
+			return;
+
+		try {
+			server = new ServerSocket(port);
+		} catch (IOException e1) {
+			  System.out.printf("%s:Error: Tried to create server but failed\n", name);
+		}
+		try {
+			client = server.accept();
+		} catch (IOException e1) {
+			  System.out.printf(
+				  "%s:Error: Error tried to connect to server but failed\n", name);
+		}
+
+		
+		while (true) {
+
+			InputStreamReader in_stream;
+			try {
+				in_stream = new InputStreamReader(client.getInputStream());
+				in = new BufferedReader(in_stream);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.printf(
+					"%s:Error: Failed to get input stream from socket\n", name);
+			}
+			
+			try {
+				data = in.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.printf(
+					"%s:Error: Failed to create an input buffer\n", name);
+			}
+			
+			if (data == null)
+				break;
+			System.out.println("Received Data: " + data);
+			printwriter.write(data); // write the message to output stream
+			printwriter.flush();
+		}
+
+		System.out.println("Shutting down the server");
+		try {
+			client.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		try {
-			server = new ServerSocket(port);
-			client = server.accept();
-			while (true) {
-
-				InputStreamReader in_stream = new InputStreamReader(client.getInputStream());
-				BufferedReader in = new BufferedReader(in_stream);
-				
-				data = in.readLine();
-				
-				if (data == null)
-					break;
-				System.out.println("Received Data: " + data);
-		
-				printwriter.write(data); // write the message to output stream
-				printwriter.flush();
-			}
-
-			System.out.println("Shutting down the server");
-			client.close();
-			producer.close();
 			server.close();
-		
-		} catch (IOException e1) {
-			  System.out.print("Server didn't get initialized\n");
-			  e1.printStackTrace();
-		}			
-		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			producer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 //	public static void main(String[] args) {
--- GUI/src/UserInterface.java
+++ GUI/src/UserInterface.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.PageAttributes.OrientationRequestedType;
 import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,122 +11,188 @@ import java.net.Socket;
 import javax.swing.*;
 
 public class UserInterface extends JFrame { 
-
+	
+	private static final int STARTED = 0;
+	private static final int STOPPED = 1;
+	private static final int RESET = 2;
+	
+	public int state = STOPPED;
+	
+	// font type for labels
+	private static final String user_font = "Arial";
+	
+	// Labels for Camera Content Panes
+	private static final String lLeftImage = " Left Eye ";
+	private static final String lRightImage = " Right Eye ";
+	private static final String lheader = " Camera Images ";
+	
+	// head orientation and control
+	private static final String lHeadOrientation = " Head Orientation Data \n";
+	private static final String lControlOptions = " ART system control options: ";
+	
+	// button labels
+	private static final String buttonLabelStart = "Start";
+	private static final String buttonLabelStop = "Stop";
+	private static final String buttonLabelReset = "Reset";
+	
+	private int hndlOrientData;
+ 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	static ServerSocket server = null;
 	
+	@SuppressWarnings("restriction")
 	public UserInterface(String name) {
 		
 		super(name);
-		JButton button_Start, button_Stop, button_Reset;
-		JLabel label_1,label_2, label_3, label_4, label_5;
-		JTextField orientation_Data, left_Eye, right_Eye;
+		final JButton button_Start;
+		JButton button_Stop, button_Reset;
+		JLabel label_1, label_2, label_3, label_4, label_5;
+		
+		final JTextArea orientation_Data;
+		
+		JTextField left_Eye, right_Eye;
 		
 	//	int NUM_SERVERS = 1;
 		
 	//	int arr[NUM_SERVERS];
 		
-		
-		
 		getContentPane().setLayout(null);
 		
-		label_1 = new JLabel(" Head Orientation Data ");
-		label_1.setFont(new Font("Serif", Font.BOLD, 16));
+		label_1 = new JLabel(lHeadOrientation);
+		label_1.setFont(new Font(user_font, Font.BOLD, 16));
 		label_1.setLocation(20, 5);
 		label_1.setSize(300, 30);
 		getContentPane().add(label_1);
 		
-		label_2 = new JLabel(" Camera Images ");
-		label_2.setFont(new Font("Serif", Font.BOLD, 16));
+		label_2 = new JLabel(lheader);
+		label_2.setFont(new Font(user_font, Font.BOLD, 16));
 		label_2.setLocation(450, 5);
 		label_2.setSize(300, 30);
 		getContentPane().add(label_2);
 		
-		label_3 = new JLabel(" Left Eye ");
-		label_3.setFont(new Font("Serif", Font.BOLD, 16));
+		label_3 = new JLabel(lLeftImage);
+		label_3.setFont(new Font(user_font, Font.BOLD, 16));
 		label_3.setLocation(350, 50);
 		label_3.setSize(300, 30);
 		getContentPane().add(label_3);
 		
-		label_4 = new JLabel(" Right Eye ");
-		label_4.setFont(new Font("Serif", Font.BOLD, 16));
+		label_4 = new JLabel(lRightImage);
+		label_4.setFont(new Font(user_font, Font.BOLD, 16));
 		label_4.setLocation(640, 50);
 		label_4.setSize(300, 30);
 		getContentPane().add(label_4);
 		
-		label_5 = new JLabel(" ART system control options: ");
-		label_5.setFont(new Font("Serif", Font.BOLD, 14));
+		label_5 = new JLabel(lControlOptions);
+		label_5.setFont(new Font(user_font, Font.BOLD, 14));
 		label_5.setLocation(20, 270);
 		label_5.setSize(300, 50);
 		getContentPane().add(label_5);
+
+//		System.out.printf("%d", hndlOrientData);
 		
-		orientation_Data = new JTextField();
+//		orientation_Data = new JTextField(lHeadOrientation);
+		orientation_Data = new JTextArea(lHeadOrientation, 5, 1);
+		orientation_Data.setLineWrap(true);
+		orientation_Data.setEditable(false);
 		orientation_Data.setLocation(5, 50);
 		orientation_Data.setSize(200, 200);
+		
+		
+//		JScrollPane scrollPane = new JScrollPane(orientation_Data);
+		hndlOrientData = orientation_Data.getComponentCount();
+//		System.out.printf("%d", hndlOrientData);
+		
+//		getContentPane().add(scrollPane);
 		getContentPane().add(orientation_Data);
 		
+		// Your going to have to change this to a JPanel most likely
 		left_Eye = new JTextField();
 		left_Eye.setLocation(250, 100);
 		left_Eye.setSize(250, 400);
 		getContentPane().add(left_Eye);
 		
+		// Your going to have to change this to a JPanel most likely
 		right_Eye = new JTextField();
 		right_Eye.setLocation(550, 100);
 		right_Eye.setSize(250, 400);
 		getContentPane().add(right_Eye);
 		
-  	    button_Start = new JButton("Start");
+  	    button_Start = new JButton(buttonLabelStart);
 	//	button_Start.addActionListener(this);
 
   	    button_Start.addActionListener(new ActionListener() {
   	    	public void actionPerformed(ActionEvent e) {
+  	    		if (state == STARTED) {
+  	    			System.out.println("System already started");
+  	    			return;
+  	    		}
+  	    		
   	    		System.out.println("Pressed Start Button");
+  	    		state = STARTED;
   	    		
     			StartServer ss = new StartServer(8001, 8000);
     			Thread th = new Thread(ss);
     			th.start();
+    			button_Start.setVisible(false);
+    			
   	    	}
   	    });
   	    
-		button_Start.setFont(new Font("Serif", Font.BOLD, 16));
+		button_Start.setFont(new Font(user_font, Font.BOLD, 16));
 		button_Start.setForeground(Color.GREEN);
 		button_Start.setLocation(45, 350);
 		button_Start.setSize(100,30);
 		getContentPane().add(button_Start);
 		
-		button_Stop = new JButton("Stop");
+		
+		button_Stop = new JButton(buttonLabelStop);
 		button_Stop.addActionListener(new ActionListener(){
   	    	public void actionPerformed(ActionEvent e) {
-  	    		StopServer(server);
-  	    		System.out.print("Hello m stop Server");
+  	    		state = STOPPED;
+//  	    		StopServer(server);
+  	    		System.out.printf("Stopping Server\n");
+  	    		button_Start.setVisible(true);
+  	    		
+  	    		orientation_Data.append("test\n");
   	 	}
   	    });
-		button_Stop.setFont(new Font("Serif", Font.BOLD, 16));
+		
+		button_Stop.setFont(new Font(user_font, Font.BOLD, 16));
 		button_Stop.setForeground(Color.RED);
 		button_Stop.setLocation(45, 400);
 		button_Stop.setSize(100,30);
 		getContentPane().add(button_Stop);
 		
-		button_Reset = new JButton("Reset");
-		button_Reset.setFont(new Font("Serif", Font.BOLD, 16));
+		button_Reset = new JButton(buttonLabelReset);
+		button_Reset.setFont(new Font(user_font, Font.BOLD, 16));
 		button_Reset.setForeground(Color.BLUE);
 		button_Reset.setLocation(45, 450);
 		button_Reset.setSize(100,30);
 		getContentPane().add(button_Reset);
 		
 	}
-
-	public static void main(String[] args) {
-		System.out.print("Art System");
+	
+	public static void createAndShowGui() {
+		System.out.println("Art System");
 		JFrame frame = new UserInterface("ART System");
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.setSize(805,600);
 		frame.setResizable(false);
 		frame.setVisible(true);
-
+	}
+	
+	@SuppressWarnings("restriction")
+	public static void main(String[] args) {
+		// Everything should operate on the "event-dispatching-thread"
+		// This ensures that no events occur prior to the GUI starting
+		javax.swing.SwingUtilities.invokeLater(new Runnable () {
+			public void run() {
+				createAndShowGui();
+			}
+		});
 	}
 	
 	
