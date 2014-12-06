14cc670d6ae3e9b0568cc388605799a8f64d90e3
Harpreet Basraon <hka55@sfu.ca>
2014-12-06 14:43:08 -0800

    test server for the ipaddress locator client

---
+++ TestServer and Client codes/src/TestServer.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TestServer {
+
+	public static void main(String[] args) {
+		
+			ServerSocket server = null;
+			
+		try {
+				server = new ServerSocket (8000);
+				Socket socket_1 = server.accept();
+			    PrintWriter out = new PrintWriter(socket_1.getOutputStream(), true);
+			    out.print("yaw");	
+	            out.close();
+			    socket_1.close();
+			} catch (IOException e1) {
+				  System.out.print("Server didn't get initialized\n");
+				  e1.printStackTrace();
+			}
+			
+		}	
+	}
+
