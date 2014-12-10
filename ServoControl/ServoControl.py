90377bd7e22a781368107526afaf05e4de94dee4
Jeremy Borys <jnickborys@gmail.com>
2014-12-10 13:58:58 -0800

    Updated Servo Motor
    Fixed Servo motor movement

--- ServoControl/ServoControl.py
+++ ServoControl/ServoControl.py
@@ -23,6 +23,7 @@ Description:
 
 import socket  # socket is the socket library that allows for commuication
 import os
+import time
 
 # HOST = '' tells python we are going to look on all available addresses. 
 # Looking at all addresses implies that python looks at the localhost ip address
@@ -63,6 +64,20 @@ if __DISTRIBUTION__ == RASBIAN:
     import RPIO.PWM as PWM
     PIN = 17
 
+
+def get_map(angle_min, angle_max, total, offset=0):
+    
+    angles = range(offset, offset + total)
+    pulses = range(angle_min, angle_max, (angle_max - angle_min)/total)
+
+    pulses_new = [] 
+    for item in pulses:
+        res = item / 10.0
+        pulses_new.append(int(round(res)) * 10)
+
+    return dict(zip(angles, pulses_new))
+
+
 def move_servo(angle):
     """
     Rashika! Here is where you would insert your code!
@@ -91,7 +106,7 @@ def set_servo(angle):
     return
 
 
-def main(connection, client_address, current_position):
+def main(connection, client_address, current_position, servo):
     """
     The main function waits for data to arrive on port 8000 and then calls
     move_servo(angle)
@@ -105,63 +120,67 @@ def main(connection, client_address, current_position):
    
     """
     if __DISTRIBUTION__ == RASBIAN:
-        servo = PWM.Servo()
+        servo.set_servo(PIN, 1500)
     print "Connected to:", client_address
 
     # ------------------------- Event Algorithm --------------------------------
-    while True:
+    try:
+        while True:
 
-        # Wait to receive data from a client, proceed when data has arrived
-        recv_angle = connection.recv(1024)
+            # Wait to receive data from a client, proceed when data has arrived
+            recv_angle = connection.recv(1024)
 
-        if recv_angle.lower() == 'whoami':
-            connection.send(WHOAMI)
-            break
+            if recv_angle.lower() == 'whoami':
+                connection.send(WHOAMI)
+                break
 
-        # If close is received terminate the server. Testing purposes only
-        if recv_angle.lower() == 'close':
-            break
+            # If close is received terminate the server. Testing purposes only
+            if recv_angle.lower() == 'close':
+                break
 
-        # Close the socket but wait for a new connection from a client
-        if not recv_angle:
-            break  # from while loop
+            # Close the socket but wait for a new connection from a client
+            if not recv_angle:
+                break  # from while loop
 
-        if not is_number(recv_angle):
-            print "ERROR: DID NOT RECEIVE A NUMBER: %s " % recv_angle
-            continue  # skip the rest of the algorithm
+            if not is_number(recv_angle):
+                print "ERROR: DID NOT RECEIVE A NUMBER: %s " % recv_angle
+                continue  # skip the rest of the algorithm
 
-        # 20 decimal places do not make sense so rounding occurs here.
-        if COORD_SYS == RELATIVE:
-            new_position = current_position + round(float(recv_angle), DECIMAL_PLACES)
-        else:
-            new_position = round(float(recv_angle), DECIMAL_PLACES)
-        
-        if not (MIN_ANGLE < new_position < MAX_ANGLE):
-            print "ERROR: NEW POSITION OUT OF BOUNDS"
+            # 20 decimal places do not make sense so rounding occurs here.
+            if COORD_SYS == RELATIVE:
+                new_position = current_position + round(float(recv_angle), DECIMAL_PLACES)
+            else:
+                new_position = round(float(recv_angle), DECIMAL_PLACES)
             
-            # Probably should update current position to Min position and Max
-            # position
+            if not (MIN_ANGLE < new_position < MAX_ANGLE):
+                print "ERROR: NEW POSITION OUT OF BOUNDS"
+                
+                # Probably should update current position to Min position and Max
+                # position
+                
+                continue
             
-            continue
-        
-        print "Moving camera by: %s degrees" % recv_angle
+            print "Moving camera by: %s degrees" % recv_angle
 
-        if __DISTRIBUTION__ == RASBIAN:
+            if __DISTRIBUTION__ == RASBIAN:
 
-            # TODO(Jeremy 2014-12-08): Determing how the angle maps to the pulse
-            #        with stored in current_position
+                # TODO(Jeremy 2014-12-08): Determing how the angle maps to the pulse
+                #        with stored in current_position
 
-            current_position = 1500  # micro seconds
-            servo.set_servo(PIN, current_position)
-        else:
-            move_servo(new_position)
+                # current_position = 1500  # micro seconds
 
-        current_position = new_position
-        print "current_position: %s" % current_position
+                servo.set_servo(PIN, R[current_position])
+            else:
+                move_servo(new_position)
 
-        servo.stop_servo(PIN)
+            current_position = new_position
+            print "current_position: %s" % current_position
+    except Exception, e:
+        print "Failed to do stuff"
+    finally:
+        # servo.stop_servo(PIN)
+        connection.close()
 
-    connection.close()
     return recv_angle
 
 
@@ -176,12 +195,24 @@ def is_number(data):
 
 if __name__ == '__main__':
 
-    access_property("delayed", "0")
-    access_property("mode", "servo")
-    access_property("servo_max", "180")
+    R = {}
+    R1 = get_map(580, 900, 45)
+    R2 = get_map(900, 1500, 45, 45)
+    R3 = get_map(1500, 2100, 45, 90)
+    R4 = get_map(2100, 2650, 45, 135)
+
+    R.update(R1)
+    R.update(R2)
+    R.update(R3)
+    R.update(R4)
+
+    # access_property("delayed", "0")
+    # access_property("mode", "servo")
+    # access_property("servo_max", "180")
     
     # NOTE THAT CODE BEGINS HERE AND CALLS THE MAIN THREAD FROM WITHIN HERE
     while True:
+        servo = PWM.Servo()
 
         # hostname = socket.gethostname()
         # IP = socket.gethostbyname(hostname)
@@ -208,7 +239,10 @@ if __name__ == '__main__':
                 cnt += 1
 
             except Exception, e:
+                time.sleep(1)
                 print "Failed to restart server"
+                if cnt >= 9:
+                    result = 'close'
 
         # Waits for single client to connect
         sock.listen(1)
@@ -216,20 +250,21 @@ if __name__ == '__main__':
         # Accept a connection from a client
         connection, client_address = sock.accept() 
 
-        access_property("active", "1")
+        # access_property("active", "1")
         print "Returning the Camera back to initial position"
         current_position = INITIAL_POSITION
         # move servo
-        move_servo(current_position)
+        # move_servo(current_position)
         
         # Enter the main loop
-        result = main(connection, client_address, current_position)
+        result = main(connection, client_address, current_position, servo)
         print "connection has stopped"
         
+        servo.stop_servo()
         del sock, connection, client_address
         if result == 'close':
             break
 
-        access_property("active", "0")
+        # access_property("active", "0")
 
     print "Closing Server"
